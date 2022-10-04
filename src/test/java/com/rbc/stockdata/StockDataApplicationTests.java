8a2a7987603889a37fc5fc51cc7f06bf3c11d133
Craig Moynes <craig.moynes@gmail.com>
2022-10-04 16:20:51 -0400

    Initial project

---
+++ src/test/java/com/rbc/stockdata/StockDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.stockdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
