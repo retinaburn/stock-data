e91a59dcdcdc3e048ea2f9c68a811a4fe5e40ab7
Craig Moynes <craig.moynes@gmail.com>
2022-10-04 18:38:43 -0400

    Add base path, and 404 on GET /id

--- src/main/java/com/rbc/stockdata/rest/RestApi.java
+++ src/main/java/com/rbc/stockdata/rest/RestApi.java
@@ -1,44 +1,50 @@
 package com.rbc.stockdata.rest;
 
-import com.rbc.stockdata.repository.StockRespository;
 import com.rbc.stockdata.model.StockData;
+import com.rbc.stockdata.repository.StockRespository;
 import com.rbc.stockdata.service.BulkInsert;
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
-@RestController
 @Slf4j
+@RestController
+@RequestMapping("api/stock-data")
 public class RestApi {
     private final StockRespository repo;
     private final BulkInsert insertService;
 
     @Autowired
     public RestApi(StockRespository repo,
-                   BulkInsert insertService){
+                   BulkInsert insertService) {
         this.repo = repo;
         this.insertService = insertService;
     }
 
     @PostMapping("bulk-insert")
-    void bulkInsert(@RequestParam("file") MultipartFile file){
+    void bulkInsert(@RequestParam("file") MultipartFile file) {
         log.debug("bulkInsert file: {}, size: {}", file.getName(), file.getSize());
 
         insertService.insertFile(file);
 
-        for (StockData item : repo.findAll()){
+        for (StockData item : repo.findAll()) {
             log.debug("Item: {}", item);
         }
     }
 
-    @GetMapping("stock-data/{id}")
-    List<StockData> getStock(@PathVariable String id){
+    @GetMapping("{id}")
+    List<StockData> getStock(@PathVariable String id) {
         log.debug("getStock {}", id);
-      return repo.findByStock(id);
+        List<StockData> stocks = repo.findByStock(id);
+
+        if (stocks.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find stock");
+        return stocks;
     }
 
 }
