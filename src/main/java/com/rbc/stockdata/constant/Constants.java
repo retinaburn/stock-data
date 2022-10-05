93ececcb80c01b113d3a6687bdafc2ad4adf1c36
Craig Moynes <craig.moynes@gmail.com>
2022-10-04 20:49:48 -0400

    Expect 16 fields (15 + 0th)

--- src/main/java/com/rbc/stockdata/constant/Constants.java
+++ src/main/java/com/rbc/stockdata/constant/Constants.java
@@ -3,5 +3,5 @@ package com.rbc.stockdata.constant;
 public class Constants {
     public static final String FIELD_DELIMITER = ",";
     public static final String HEADER_CLIENTID = "X-Client_Id";
-    public static final int EXPECTED_NUM_FIELDS = 15;
+    public static final int EXPECTED_NUM_FIELDS = 16;
 }
